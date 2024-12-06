135d03c81aeef57ca67ee4c3798023f774ef5119
Phan Tấn Dũng <dungpt@vinades.vn>
2024-12-06 11:16:24 +0700

    Chuyển giao diện error_info

--- src/themes/future/theme_error_info.php
+++ src/themes/future/theme_error_info.php
@@ -21,83 +21,73 @@ if (!defined('NV_MAINFILE')) {
 $errortype = [
     E_ERROR => [
         $nv_Lang->getGlobal('error_error'),
-        'bad.png'
+        'danger'
     ],
     E_WARNING => [
         $nv_Lang->getGlobal('error_warning'),
-        'warning.png'
+        'warning'
     ],
     E_PARSE => [
         $nv_Lang->getGlobal('error_error'),
-        'bad.png'
+        'danger'
     ],
     E_NOTICE => [
         $nv_Lang->getGlobal('error_notice'),
-        'comment.png'
+        'info'
     ],
     E_CORE_ERROR => [
         $nv_Lang->getGlobal('error_error'),
-        'bad.png'
+        'danger'
     ],
     E_CORE_WARNING => [
         $nv_Lang->getGlobal('error_warning'),
-        'warning.png'
+        'warning'
     ],
     E_COMPILE_ERROR => [
         $nv_Lang->getGlobal('error_error'),
-        'bad.png'
+        'danger'
     ],
     E_COMPILE_WARNING => [
         $nv_Lang->getGlobal('error_warning'),
-        'warning.png'
+        'warning'
     ],
     E_USER_ERROR => [
         $nv_Lang->getGlobal('error_error'),
-        'bad.png'
+        'danger'
     ],
     E_USER_WARNING => [
         $nv_Lang->getGlobal('error_warning'),
-        'warning.png'
+        'warning'
     ],
     E_USER_NOTICE => [
         $nv_Lang->getGlobal('error_notice'),
-        'comment.png'
+        'info'
     ],
     E_STRICT => [
         $nv_Lang->getGlobal('error_notice'),
-        'comment.png'
+        'info'
     ],
     E_RECOVERABLE_ERROR => [
         $nv_Lang->getGlobal('error_error'),
-        'bad.png'
+        'danger'
     ],
     E_DEPRECATED => [
         $nv_Lang->getGlobal('error_notice'),
-        'comment.png'
+        'info'
     ],
     E_USER_DEPRECATED => [
         $nv_Lang->getGlobal('error_warning'),
-        'warning.png'
+        'warning'
     ]
 ];
 
 $tpl_dir = get_tpl_dir($php_dir, 'default', '/system/error_info.tpl');
-$image_path = NV_STATIC_URL . 'themes/' . $tpl_dir . '/images/icons/';
 
-$xtpl = new XTemplate('error_info.tpl', NV_ROOTDIR . '/themes/' . $tpl_dir . '/system');
-$xtpl->assign('TPL_E_CAPTION', $nv_Lang->getGlobal('error_info_caption'));
+$tpl = new \NukeViet\Template\NVSmarty();
+$tpl->setTemplateDir(NV_ROOTDIR . '/themes/' . $tpl_dir . '/system');
+$tpl->assign('LANG', $nv_Lang);
+$tpl->assign('ERRORTYPE', $errortype);
+$tpl->assign('ERROR_INFO', $error_info);
+$tpl->assign('TEMPLATE', $tpl_dir);
 
-$a = 0;
-foreach ($error_info as $key => $value) {
-    $xtpl->assign('TPL_E_CLASS', ($a % 2) ? ' class="second"' : '');
-    $xtpl->assign('TPL_E_ALT', $errortype[$value['errno']][0]);
-    $xtpl->assign('TPL_E_SRC', $image_path . $errortype[$value['errno']][1]);
-    $xtpl->assign('TPL_E_ERRNO', $errortype[$value['errno']][0]);
-    $xtpl->assign('TPL_E_MESS', $value['info']);
-    $xtpl->set_autoreset();
-    $xtpl->parse('error_info.error_item');
-    ++$a;
-}
-
-$xtpl->parse('error_info');
-return $xtpl->text('error_info');
+return $tpl->fetch('error_info.tpl');
